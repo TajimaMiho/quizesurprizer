67908e4b268de22390ea86bf451bae531f30d84f
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-11-29 23:42:24 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -12,11 +12,7 @@ let speechOut;
 var num;
 const makeStateSpeach = function(){
     num = Math.floor(Math.random()*ReturnStrArr.length);
-    speechOut = "<audio src='https://path/xxx.mp3' />"
-    speechOut += "問題。！";
-    speechOut += ReturnStrArr[num][0];
-   // return '<audio src="s3://musicquiz34/Question.mp3"/>' +"問題。"+ ReturnStrArr[num][0];
-   return speechOut;
+   return '<audio src="s3://musicquiz34/Question.mp3"/>' +"問題。"+ ReturnStrArr[num][0];
 }
 //s3://28d838a1-22d7-47ec-acc6-f239904f397a-us-east-1/Media/マイムービー.mp3
 //'https://28d838a1-22d7-47ec-acc6-f239904f397a-us-east-1.s3.amazonaws.com/Media/%E3%83%9E%E3%82%A4%E3%83%A0%E3%83%BC%E3%83%93%E3%83%BC.mp3'
