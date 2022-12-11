9d26ff7909b49378ecf3348029cf43e81918c003
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-12-11 03:01:35 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -60,7 +60,7 @@ const QuizMorningIntentHandler = {
             attr.lastCount = count;
         handlerInput.attributesManager.setPersistentAttributes(attr);
             await handlerInput.attributesManager.savePersistentAttributes();
-            speakOutput = '<audio src="soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_tally_positive_01"/>'+"正解。"+"そうそう。今日もあなたの活躍のおかげでで進捗があったみたいですよ。聞きたければ合言葉を言ってください。";
+            speakOutput = '<audio src="soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_tally_positive_01"/>'+"正解。"+"そうそう。あなたの活躍のおかげで今日も進捗があったみたいですよ。聞きたければ合言葉を言ってください。";
             return handlerInput.responseBuilder
             .speak(speakOutput)
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
