1f2265799228c749544fa8693b76c6c8b35536bb
TajimaMiho <kapibaramihho@gmail.com>
2022-12-16 02:49:29 +0900

    ラインと繋げた。クイズ増えた

--- lambda/index.js
+++ lambda/index.js
@@ -4,6 +4,8 @@
  * session persistence, api calls, and more.
  * */
 const Alexa = require('ask-sdk-core');
+const Axios = require('axios');
+const API_URL = 'https://script.google.com/macros/s/AKfycbyt5SMXQutt9fRBWuEMnaBMnYaAc84MUOgct7vNSzHqbPWlbG34tY32BwNjE9FE0y6v7g/exec';
 //["問題文","ヒント1","ヒント2","解答例1","解答例2","解答例3"]
 const ReturnStrArr=　[
   //["イルカとくじらひっくり返ったらどっちが軽い？", "ひっくり返るんだよ？", "文字を逆さにしてみたら？","イルカ", "いるか", "海豚"],
@@ -16,14 +18,26 @@ const ReturnStrArr=　[
   ['<audio src="soundbank://soundlibrary/vehicles/trains/train_05"/>'+"この電車、何回鈴なった？", "8回以上14回未満","10回から12回",
   '十', '十一', '十二'],
   ["なかなか掴めなくて、イライラするお肉ってなんだ。次の鳴き声の中から選んでください。答え方は、牛肉、のように、なんちゃら肉、という形で答えよ。" + '<audio src="soundbank://soundlibrary/animals/amzn_sfx_chicken_cluck_01"/>' + '<audio src="soundbank://soundlibrary/animals/amzn_sfx_sheep_bleat_01"/>' + '<audio src="soundbank://soundlibrary/animals/amzn_sfx_horse_whinny_02"/>', "掴めない。掴めない。他の言い方をするとー。","掴めない。掴めない。とりにくいねぇ。", "鶏肉", "トリ肉", "とりにく"],
+  ["今からならすエンジンのうち、バイクを選べ。一。" + '<audio src="soundbank://soundlibrary/vehicles/cars/cars_02"/>' + "二" + '<audio src="soundbank://soundlibrary/vehicles/buses/buses_08"/>' + '三' + '<audio src="soundbank://soundlibrary/transportation/amzn_sfx_motorcycle_engine_rev_01"/>', "よく聞いてください", "よく聞いてね", "三", "三つ目", "最後"],
   ['ドアが3つある。見た目は一緒。木製をとおるべし。よく耳をすませ。' + 'まずは左から。' + '<audio src="soundbank://soundlibrary/doors/doors_prison/prison_02"/>' + '次に真ん中' + '<audio src="soundbank://soundlibrary/doors/doors_metal/metal_04"/>'
   + '最後に右' + '<audio src="soundbank://soundlibrary/doors/doors_wood/wood_04"/>'  + '正しいドアはどれ？', 'よく聞いてみて', 'みしって聞こえる？', '右' , 'みぎ' , '三'],
   ["シャンデリアシャンデリアシャンデリアシャンデリアシャンデリアシャンデリアシャンデリアシャンデリアシャンデリアシャンデリア。ガラスの靴を拾ったのは？", "拾った人ですよ？", "ガラスの靴を拾ったのはシンデレラではありませんよね？","王子様", "王子", "おうじさま"],
+  ["みりんみりんみりんみりんみりんみりんみりんみりんみりんみりん。はなが長いのは？", "長いのはクビじゃないよ？", "はなが長い動物", "ぞう", "象", "ゾウ"],
+  ["あしたは雨が降りそうです。でも大丈夫です。あしかがきっとあしたの雨を飲み込んでくれるはずです。さて、私は何回、あした、と言ったでしょう？", "二回", "二", "に"],
+  ['箱の中身はなんでしょう？お題は野菜です！'+ '<audio src="soundbank://soundlibrary/doors/doors_handles/handle_05"/>'+ '細長い。けど、割としっかりしてる。ん〜と、表面がツルツルしてるー。', '色は緑と白。', '長さは40センチメートルくらい。', 'ねぎ', 'ネギ', '葱'],
+  ["私の思い浮かべているものはなんでしょう？お題はゆうぐです！じゅんびはいいですか？鉄の棒でできています。大きくて四角いです。さあ、なんでしょう？", "登ります", "最近は危ないと言われています。", "ジャングルジム", "じゃんぐるじむ", "グローブジャングル"],
+  ["今からならすエンジンのうち、車を選べ。一。" + '<audio src="soundbank://soundlibrary/vehicles/cars/cars_02"/>' + "二" + '<audio src="soundbank://soundlibrary/vehicles/buses/buses_08"/>' + '三' + '<audio src="soundbank://soundlibrary/transportation/amzn_sfx_motorcycle_engine_rev_01"/>', "よく聞いてください", "よく聞いてね", "一", "一つ目目", "最初"],
+  ['ドアが3つある。見た目は一緒。メタルでできたものをとおるべし。よく耳をすませ。' + 'まずは左から。' + '<audio src="soundbank://soundlibrary/doors/doors_prison/prison_02"/>' + '次に真ん中' + '<audio src="soundbank://soundlibrary/doors/doors_metal/metal_04"/>'
+  + '最後に右' + '<audio src="soundbank://soundlibrary/doors/doors_wood/wood_04"/>'  + '正しいドアはどれ？', 'よく聞いてみて', 'みしって聞こえる？', '真ん中' , '中' , '二'],
+  ['思ってもない展開だな。でも私は天才肌なので限界だなを踏み越える。さて、私は今何回韻を踏んだでしょう？', '韻とは似たような発音の言葉です。', 'よく聞いてね','三回', '三', '３'],
+  ["次のうち、煙を出して走っている電車はどれ？一。", '<audio src="soundbank://soundlibrary/vehicles/trains/train_03"/>', "二", '<audio src="soundbank://soundlibrary/vehicles/trains/train_07"/>', '三', '<audio src="soundbank://soundlibrary/vehicles/trains/train_06"/>', 'よく聞いてね', 'よく聞いてね', '一', '一回目', '最初'],
+  [""],
 ];
 const Serifu = ["おはようございます。今日もいい日ですね。ところで、あなたが今日早起きしてくれたことで危機が一つ去りました。ネムール伯爵の使者が一人去ったのです。え？そんなシステムだったのかって？そうですよ、あなたが早起きすればするほど、ネムール伯爵が弱っていくのです。。。おっと、失礼しました。私はあなたの味方ですのでご安心ください。",
 "ネムール伯爵を倒すと決意してから早二日ですね...今日もしっかり起きてくださりありがとうございます。本日も使者が一人去って行きました...今日は彼の護衛の騎士が去っていきました。この使者は私の事を快く思っていないらしく、毎日嫌がらせをしてくるのです。しかし、そんな事でへこたれる私ではありません！ 昨夜も寝る前に魔法を使いながら精神統一し、今日こそは必ず勝つという強い意志で眠りにつきましたからね！", 
 "おはようございます！勇者様！今日は勇者様に朗報があるんですよ...なんと！私アレクサ、魔法を使えるようになったのです！！え？どんな魔法なのかって？火です、火。音だけじゃ微妙ですか？でもでも安心してください！もうすでに練習もしましたから！" + '<audio src="soundbank://soundlibrary/explosions/fireballs/fireballs_10"/>' + "明日からのクイズにはこんなのも使っちゃいますよー！", 
-"し", "ご", "ろく", "なな", "はち", "きゅう", "じゅう"];
+"おはようございます、アレクサです。今日で早起き四日目ですね！今日の進捗はですね...今日も敵が一人減りましたよ！え？敵が減ったって毎日言われても実感が湧かない？仕方ないですね...じゃあ昨日倒したゴブリンの数を教えてあげましょう。なんと五匹ですよ！！私一人で5匹のゴブリンを倒したんですよ！これはかなり凄い事なんですからね！", 
+"ご", "ろく", "なな", "はち", "きゅう", "じゅう"];
 var num;
 var count = 0;
 var hint = 0;
@@ -44,7 +58,7 @@ const LaunchRequestHandler = {
         const lastCount = attr.lastCount;
         var speakOutput;
         if (lastCount !== undefined) count = lastCount;
-        if(count === 0)  speakOutput = 'この世界にいつか到来すると言われている、ネムール伯爵。彼は、人間が早起きせずに怠惰にしている時に、ひどく活性化すると言われているのです。彼の野望を止めるために、勇者様には、毎日早起きして欲しいのです。そういえば、大事なことを言い忘れていました。この世界での合言葉は、ひらけごま、です。絶対に忘れないでくださいね。' + makeStateSpeach();
+        if(count === 0)  speakOutput = 'おはようございます、今日もいい朝、そう思ってませんか/？..'+ '<audio src="soundbank://soundlibrary/guns/cannons/cannons_02"/>' +'そんな幻想も今日で終わりです。その原因はこの世界にいつか到来すると言われている、ネムール伯爵。彼は、人間が早起きせずに怠惰にしている時に、ひどく活性化すると言われているのです。彼の野望を止めるために、勇者様には、毎日早起きして欲しいのです。' + makeStateSpeach();
         else speakOutput = makeStateSpeach();
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -68,7 +82,7 @@ const QuizMorningIntentHandler = {
         handlerInput.attributesManager.setPersistentAttributes(attr);
             await handlerInput.attributesManager.savePersistentAttributes();
             if(count<4){
-                speakOutput = '<audio src="soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_tally_positive_01"/>' + "正解。" + "そう、そう。あなたの活躍のおかげで今日も進捗があったみたいですよ？聞きたければ合言葉を言ってください。";
+                speakOutput = '<audio src="soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_tally_positive_01"/>' + "正解。" + "そう、そう。あなたの活躍のおかげで今日も進捗があったみたいですよ？聞きたいですか？";
                 return handlerInput.responseBuilder
                 .speak(speakOutput)
                 .reprompt()
@@ -115,13 +129,18 @@ const StoryIntentHandler = {
             && handlerInput.requestEnvelope.request.intent.name === 'StoryIntent';
     },
     handle(handlerInput) {
-        const speechText = Serifu[count-2];
-
+        var answer = handlerInput.requestEnvelope.request.intent.slots.bool.value;
+        var speechText;
+        if(answer === "はい" || answer === "うん" || answer === "聞く" || answer === "聞きたい" || answer === "聞きたいです")
+        speechText = Serifu[count-1] + '今日の進捗はこれで終わりです。今日もよい一日を';
+        else speechText = '今日もよい一日を';
+        Axios.post(API_URL, Serifu[count-1]);
         return handlerInput.responseBuilder
-            .speak(Serifu[count-1] + '今日の進捗はこれで終わりです。今日もよい一日を')
+            .speak(speechText)
             .getResponse();
     }
 };
+
 
 
 
